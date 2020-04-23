da990f80466a4e0ee4a8d8b57f0698625f5d73a5
zimbatm <zimbatm@zimbatm.com>
2020-04-23 22:58:07 +0200

    ci: fix the archive names

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
+system=$(nix-instantiate --eval --expr builtins.currentSystem | jq -r)
+archive_name=bash-${system}.tar.gz
+archive_path=build/$archive_name
+bin_name=bash-${system}
+bin_path=build/$bin_name
+
+# Build
 rm -rf build
 mkdir -p build
-nix-build -A archive -o build/bash.tar.gz
-nix-build -A bin -o build/bash
+
+nix-build -A archive -o "$archive_path"
+nix-build -A bin -o "$bin_path"
+
+# Set outputs
+echo "::set-output name=archive-name::$archive_name"
+echo "::set-output name=archive-path::$archive_path"
+echo "::set-output name=bin-name::$bin_name"
+echo "::set-output name=bin-path::$bin_path"
