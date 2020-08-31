86cdb0f879af8baff2c2721d575bbe15ed07bd24
zimbatm <zimbatm@zimbatm.com>
2020-09-01 01:33:40 +0200

    refactor build upload

--- build.sh
+++ build.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-system=$(nix-instantiate --eval --expr builtins.currentSystem | xargs)
-archive_name=bash-${system}.gz
-archive_path=build/$archive_name
-bin_name=bash-${system}
-bin_path=build/$bin_name
-
-# Build
+# Build directory
 rm -rf build
 mkdir -p build
 
-nix-build -A archive -o "$archive_path"
-nix-build -A bin -o "$bin_path"
-
-# Set outputs
-echo "::set-output name=archive-name::$archive_name"
-echo "::set-output name=archive-path::$archive_path"
-echo "::set-output name=bin-name::$bin_name"
-echo "::set-output name=bin-path::$bin_path"
+for attr in $(nix-instantiate --eval --expr '(builtins.attrNames (import ./.  {}))' --json | jq -r .[]) ; do
+  echo "--- attr=$attr"
+  nix-build -A "$attr" -o ./result
+  filename=$(readlink ./result | cut -d - -f 2-)
+  cp -rv "$(readlink ./result)" "./build/$filename"
+done
