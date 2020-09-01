0b318f9413abd13a49d83565001e7e49efb62e03
zimbatm <zimbatm@zimbatm.com>
2020-09-01 12:12:48 +0200

    refactor build upload (#1)

--- default.nix
+++ default.nix
@@ -2,20 +2,7 @@
 let
   system = pkgs.system;
 
-  bashOrig =
-    if pkgs.stdenv.isDarwin then
-      # musl is not available on darwin
-      pkgs.bashInteractive
-    else
-      pkgs.pkgsStatic.bashInteractive;
-
-  bash = bashOrig.overrideAttrs
-    (attrs: {
-      postFixup = ''
-        rm -rf "$out/bin/sh" "$out/share" "$out/bin/bashbug"
-      '';
-    });
-
+  # Extract the main binary of the package and copy it to the root
   mkBin = name: drv:
     pkgs.runCommand
       "${name}-${system}"
@@ -23,14 +10,50 @@ let
       "cp ${drv}/bin/${name} $out";
 
   # Creates a reproducible archive
-  mkGzip = name: drv:
+  mkBinGzip = name: drv:
     pkgs.runCommand
       "${name}-${system}.gz"
       { nativeBuildInputs = [ pkgs.gzip ]; }
       "gzip --no-name --stdout ${drv}/bin/${name} > $out";
+
+  bash =
+    let
+      orig =
+        if pkgs.stdenv.isDarwin then
+        # musl is not available on darwin
+          pkgs.bashInteractive
+        else
+          pkgs.pkgsStatic.bashInteractive;
+    in
+    orig.overrideAttrs
+      (attrs: {
+        postFixup = ''
+          rm -rf "$out/bin/sh" "$out/share" "$out/bin/bashbug"
+        '';
+      });
+
+  coreutils =
+    let
+      orig =
+        if pkgs.stdenv.isDarwin then
+        # musl is not available on darwin
+          pkgs.coreutils
+        else
+          pkgs.pkgsStatic.coreutils;
+    in
+    orig.overrideAttrs
+      (attrs: {
+        postFixup = ''
+          rm -rf "$out/nix-support"
+        '';
+      });
 in
 {
-  inherit bash;
-  archive = mkGzip "bash" bash;
-  bin = mkBin "bash" bash;
+  # inherit bash;
+  bash-bin = mkBin "bash" bash;
+  bash-bin-gzip = mkBinGzip "bash" bash;
+
+  # inherit coreutils;
+  coreutils-bin = mkBin "coreutils" coreutils;
+  coreutils-bin-gzip = mkBinGzip "coreutils" coreutils;
 }
